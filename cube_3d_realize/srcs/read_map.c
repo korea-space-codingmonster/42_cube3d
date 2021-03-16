fb2c333ccb9d836c30bae36cd3698f0d9fb45a9d
NAMGYU PARK <namgyupark@MacBook-Pro.local>
2021-03-16 15:49:45 +0900

    add

--- cube_3d_realize/srcs/read_map.c
+++ cube_3d_realize/srcs/read_map.c
@@ -6,7 +6,7 @@
 /*   By: napark <napark@studenst.42seoul.kr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 12:35:34 by napark            #+#    #+#             */
-/*   Updated: 2021/03/16 14:42:29 by napark           ###   ########.fr       */
+/*   Updated: 2021/03/16 15:49:37 by napark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,34 @@ int     decide_store_what_info(t_all *s, char **info)
 {
     if (ft_strcmp(info[0], "R") == 0)
         store_map_size(s, info[1], info[2]);
+    else if (ft_strcmp(info[0], "NO") == 0)
+        store_location_path(s, info[1], NORTH);
         
 }
 
 void    store_map_size(t_all *s, char *width, char *height)
 {
     if (!width || !height)
-        ft_strexit("ERROR : Empty width height. Please check your cubs file");
+        ft_strexit("ERROR : Empty width height. Please check your .cubs file");
+    if (!ft_only_digit(width) && !ft_only_digit(height))
+        ft_strexit("ERROR : Either one of them is not a number.");
+    s->width = ft_atoi(width);
+    s->height = ft_atoi(height);
+    s->width = s->width > 1920 ? 1920 : s->width;
+    s->width = s->width < 848 ? 848 : s->width;
+    s->height = s->height > 1080 ? 1080 : s->height;
+    s->height = s->height < 480 ? 480 : s->height;
+    
+}
+
+void    store_location(t_all *s, char *path, int location)
+{
+    int     fd;
+
+    if (!path || !ft_isformat(path, ".xpm"))
+        ft_strexit("ERROR : Path is empty or path is Invalid format");
+    
+    fd = open(path, O_RDONLY);
+    if (fd == -1)
+        ft_strexit("ERROR : File(.xpm) is not exisit");
 }
